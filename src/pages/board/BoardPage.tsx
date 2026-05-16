0e21c700071ea83fb0f28a59555362983683f857
p1001q <qrwrer123@gmail.com>
2026-05-17 02:48:27 +0900

    refactor: 검색 버튼 컴포넌트 제거

--- src/pages/board/BoardPage.tsx
+++ src/pages/board/BoardPage.tsx
@@ -1,12 +1,3 @@
-import SearchButton from '../../components/button/SearchButton'
-
 export default function BoardPage() {
-  return (
-    <div>
-      <header className="flex items-center justify-end px-4 h-14 bg-gray1">
-        <SearchButton />
-      </header>
-      <main>게시판</main>
-    </div>
-  )
+  return <div>게시판</div>
 }
