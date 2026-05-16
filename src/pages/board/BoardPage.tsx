17aea19d76aaf5473d937bd58fdfee2b51ced4c5
p1001q <qrwrer123@gmail.com>
2026-05-17 02:39:27 +0900

    feat: 게시판 페이지 헤더에 검색 버튼 추가

--- src/pages/board/BoardPage.tsx
+++ src/pages/board/BoardPage.tsx
@@ -1,3 +1,12 @@
+import SearchButton from '../../components/button/SearchButton'
+
 export default function BoardPage() {
-  return <div>게시판</div>
+  return (
+    <div>
+      <header className="flex items-center justify-end px-4 h-14 bg-gray1">
+        <SearchButton />
+      </header>
+      <main>게시판</main>
+    </div>
+  )
 }
