c9a74162b14a5de6f7bd43b24763af80958f00fd
p1001q <qrwrer123@gmail.com>
2026-05-17 04:08:11 +0900

    fix: 뒤로가기 아이콘 적용 문제 해결

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import PostCard from '../../components/card/ListCard'
 import { searchPosts, type Post } from '../../apis/search/search'
 import SearchIcon from '../../assets/icons/search.svg?react'
+import BackIcon from '../../assets/icons/back.svg?react'
 
 export default function SearchPage() {
   const navigate = useNavigate()
@@ -69,12 +70,7 @@ export default function SearchPage() {
           className="absolute left-7 flex h-6 w-6 shrink-0 items-center justify-center"
           aria-label="뒤로가기"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path
-              d="M14.293 5.29295C14.6835 4.90243 15.3165 4.90243 15.707 5.29295C16.0975 5.68348 16.0975 6.31649 15.707 6.70702L10.414 12L15.707 17.293C16.0975 17.6835 16.0975 18.3165 15.707 18.707C15.3165 19.0975 14.6835 19.0975 14.293 18.707L8.29295 12.707C7.90243 12.3165 7.90243 11.6835 8.29295 11.293L14.293 5.29295Z"
-              fill="white"
-            />
-          </svg>
+          <BackIcon className="h-6 w-6 text-white" />
         </button>
         <h1 className="text-base font-medium text-white">검색하기</h1>
       </header>
