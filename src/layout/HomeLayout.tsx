82177de6cd09203e93c7879b40bbfaa18417d0cc
nue-os <kimsoeun0108@gmail.com>
2026-05-17 01:59:19 +0900

    feat: 홈 페이지 레이아웃 헤더 적용

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import BaseLayout from './BaseLayout'
+import Logo from '../assets/icons/logo.svg?react'
+import SearchIcon from '../assets/icons/search.svg?react'
 
 export default function HomeLayout() {
   return (
     <BaseLayout>
+      <header className="relative flex items-center justify-center py-2.5">
+        <Logo className="w-20" />
+        <SearchIcon className="text-gray3 absolute right-7" />
+      </header>
       <Outlet />
     </BaseLayout>
   )
